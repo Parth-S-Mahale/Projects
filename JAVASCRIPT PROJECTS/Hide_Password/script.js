377d009b23e4ed7c424d6d5f3b80cbf014fa7541
Parth S. Mahale <parthmahale05@gmail.com>
2024-09-17 12:39:12 +0530

    project2

---
+++ JAVASCRIPT PROJECTS/Hide_Password/script.js
@@ -0,0 +1,13 @@
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeicon.onclick = function(){
+    if(password.type == "password"){
+        password.type = "text";
+        eyeicon.src = "images/eye-open.png"
+        
+    } else{
+        password.type = "password";
+        eyeicon.src = "images/eye-close.png"
+    }
+}
